6fe5dc60a3da5068753ecc98ee3f45c40549fb40
dhruv12ka4 <125910898+dhruv12ka4@users.noreply.github.com>
2023-03-22 16:19:39 +0530

    Add files via upload

---
+++ harsh.python.py
@@ -0,0 +1,12 @@
+class point:
+
+    def __init__(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def sqsum(self):
+        return self.x*self.x + self.y*self.y + self.z*self.z
+
+p = point(1,3,5)
+print(p.sqsum())
